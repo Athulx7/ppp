3323b23b9fd91fd6e1adad0aaf8c5e6d53788dc7
Athulx7 <athulkrishnapr2004@gmail.com>
2026-03-11 22:51:31 +0530

    hhh

--- src/AnimatedLandingPage/AnimatelandingPage.jsx
+++ src/AnimatedLandingPage/AnimatelandingPage.jsx
@@ -191,7 +191,7 @@ function AnimatelandingPage() {
                 </h1>
 
                 <p className="text-gray-500 text-lg max-w-3xl leading-relaxed mt-4">
-                    {/* From punch-in to payslips — a complete HRMS platform for employees, HR teams, and payroll managers. Multi-tenant, secure, and lightning fast. */}
+                    {/* From punch-in to payslips — a complete HRMS platform for employees, HR teams, and payroll managers. Multi-tenant, secure, and lightning fast. */} 
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum sed nobis consequuntur voluptatem aperiam natus voluptate earum animi, maiores quia
                 </p>
 
